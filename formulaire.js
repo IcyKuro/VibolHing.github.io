09e940a258960fa84423482daa7ba17aa957f9cf
IcyKuro <98219897+IcyKuro@users.noreply.github.com>
2025-05-17 11:50:07 +0200

    Add files via upload
    V2

---
+++ formulaire.js
@@ -0,0 +1,13 @@
+document.getElementById("contactForm").addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    const email = document.getElementById("emailAddress").value;
+    const sujet = document.getElementById("sujet").value;
+    const message = document.getElementById("message").value;
+
+    const gmailLink = `https://mail.google.com/mail/?view=cm&fs=1&to=hingvibol@gmail.com&su=${encodeURIComponent(
+      sujet
+    )}&body=${encodeURIComponent(`Email de l'utilisateur : ${email}\n\n${message}`)}`;
+
+    window.open(gmailLink, '_blank'); // ouvre dans un nouvel onglet
+  });
