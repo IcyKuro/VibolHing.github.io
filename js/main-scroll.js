0d7d1c7bf67d8758cf9210cd7422a7da1754eafc
Vibol <hingvibo@gmail.com>
2026-02-10 22:21:45 +0100

    New effect + Responsive fix + Project Page template done

--- js/main-scroll.js
+++ js/main-scroll.js
@@ -14,24 +14,21 @@ const section3 = document.querySelector('#sec3');
 const lineProgress = document.querySelector('.line-progress');
 const timelineWrapper = document.querySelector('.timeline-wrapper');
 const stars = document.querySelectorAll('.star');
-
+const body = document.body;
 function updateTimelineScroll() {
   if (!section3 || !lineProgress || !timelineWrapper) return;
 
   const rect = timelineWrapper.getBoundingClientRect();
+  const sectionRect = section3.getBoundingClientRect();
   const windowHeight = window.innerHeight;
 
   let progress = (windowHeight / 2 - rect.top) / rect.height;
   progress = Math.max(0, Math.min(1, progress));
-
   lineProgress.style.height = `${progress * 100}%`;
 
   const currentLineHeight = rect.height * progress;
-
   stars.forEach(star => {
-    
     const starRect = star.getBoundingClientRect();
-    
     const starPositionInWrapper = starRect.top - rect.top;
 
     if (currentLineHeight >= starPositionInWrapper + (starRect.height / 2)) {
@@ -40,6 +37,18 @@ function updateTimelineScroll() {
       star.classList.remove('active');
     }
   });
+
+  /* NIGHT MODE SWITCH */
+  const triggerPointStart = windowHeight * 0.2;
+  const startNight = sectionRect.top < triggerPointStart;
+  const triggerPointEnd = windowHeight * 0.1;
+  const endNight = sectionRect.bottom > triggerPointEnd;
+
+  if (startNight && endNight) {
+    body.classList.add('night-mode');
+  } else {
+    body.classList.remove('night-mode');
+  }
 }
 
 function raf(time) {
